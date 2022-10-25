a7b1607f9a1b7f40d74343f6ac867e5af87f29b8
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2022-10-24 22:11:20 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -62,7 +62,6 @@ def pregunta_03():
     Especificación del pipeline y entrenamiento
     -------------------------------------------------------------------------------------
     """
-
     # Importe make_column_selector
     # Importe make_column_transformer
     # Importe SelectKBest
@@ -71,7 +70,6 @@ def pregunta_03():
     # Importe GridSearchCV
     # Importe Pipeline                          OK
     # Importe OneHotEncoder
-    
     from sklearn.compose import make_column_selector
     from sklearn.compose import make_column_transformer
     from sklearn.feature_selection import SelectKBest
@@ -80,13 +78,12 @@ def pregunta_03():
     from sklearn.model_selection import GridSearchCV
     from sklearn.pipeline import Pipeline
     from sklearn.preprocessing import OneHotEncoder
-
     pipeline = Pipeline(
-       steps=[
-           # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
-           # variables categóricas, y no aplica ninguna transformación al resto de
-           # las variables.
-           (
+        steps=[
+            # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
+            # variables categóricas, y no aplica ninguna transformación al resto de
+            # las variables.
+            (
                "column_transfomer",
                make_column_transformer(
                    (
@@ -95,47 +92,47 @@ def pregunta_03():
                    ),
                    remainder='passthrough',
                ),
-           ),
-           # Paso 2: Construya un selector de características que seleccione las K
-           # características más importantes. Utilice la función f_regression.
-           (
-               "selectKBest",
-               SelectKBest(score_func=f_regression),
-           ),
-           # Paso 3: Construya un modelo de regresión lineal.
-           (
+            ),
+            # Paso 2: Construya un selector de características que seleccione las K
+            # características más importantes. Utilice la función f_regression.
+            (
+                "selectKBest",
+                SelectKBest(score_func=f_regression),
+            ),
+            # Paso 3: Construya un modelo de regresión lineal.
+            (
                "LR",
                LinearRegression(),
-           ),
-       ],
-   )
+            ),
+        ],
+    )
 
-   # Cargua de las variables.
-   X_train, X_test, y_train, y_test  = pregunta_02()
+    # Cargua de las variables.
+    X_train, X_test, y_train, y_test  = pregunta_02()
 
-   # Defina un diccionario de parámetros para el GridSearchCV. Se deben
-   # considerar valores desde 1 hasta 11 regresores para el modelo
-   param_grid = {
-       "selectKBest__k": (1, 11),
-   }
+    # Defina un diccionario de parámetros para el GridSearchCV. Se deben
+    # considerar valores desde 1 hasta 11 regresores para el modelo
+    param_grid = {
+        "selectKBest__k": (1, 11),
+    }
 
-   # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
-   # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
-   # error cuadrático medio.
-   gridSearchCV = GridSearchCV(
-       estimator=pipeline,
-       param_grid=param_grid,
-       cv=5,
-       scoring="neg_mean_squared_error",
-       refit=True,
-       return_train_score=False,
-   )
+    # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
+    # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
+    # error cuadrático medio.
+    gridSearchCV = GridSearchCV(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv=5,
+        scoring="neg_mean_squared_error",
+        refit=True,
+        return_train_score=False,
+    )
 
-   # Búsque la mejor combinación de regresores
-   gridSearchCV.fit(X_train, y_train)
+    # Búsque la mejor combinación de regresores
+    gridSearchCV.fit(X_train, y_train)
 
-   # Retorne el mejor modelo
-   return gridSearchCV
+    # Retorne el mejor modelo
+    return gridSearchCV
 
 """
    Evaluación del modelo
